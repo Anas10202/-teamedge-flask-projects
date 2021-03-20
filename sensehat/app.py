2799a090fab81e974368f8854c3b9bac144a4481
Anas10202 <77549785+Anas10202@users.noreply.github.com>
2021-03-20 14:20:09 -0400

    Add files via upload

--- sensehat/app.py
+++ sensehat/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template , redirect, request,url_for,json ,jsonify, current_app as app
 from datetime import date
+from sense_hat import SenseHat
 import requests
 import os
 
 app = Flask(__name__)
+sense = SenseHat()
 
 @app.route('/success/<name>')
 def success(name):
-    return 'welcome %s' % name
+    return name,sense.show_message("welcome"+name)
 
 @app.route('/login', methods = ['POST','GET'])
 def login():
@@ -16,8 +18,7 @@ def login():
         return redirect(url_for('success', name = user))
     else:
         user = request.args.get('nm')
-        return redirect(url_for('success', name = user))
-
+        return render_template('login.html', name = user)
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
