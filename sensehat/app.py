82d2427db4f19822fef1c77ef5ebb48ec305d36d
Anas10202 <77549785+Anas10202@users.noreply.github.com>
2021-04-17 11:34:43 -0400

    Add files via upload

--- sensehat/app.py
+++ sensehat/app.py
@@ -8,23 +8,42 @@ import sqlite3
 app = Flask(__name__)
 sense = SenseHat()
 
-@app.route('/received/<name>')
-def success(name):
-    return name,sense.show_message(name)
+@app.route('/received/<message>')
+def success(message):
+    return message
 
 @app.route('/message', methods = ['POST','GET'])
 def login():
     if request.method == 'POST':
-        user= request.form['nm']
+        message= request.form['nm']
+        sense.show_message(message)
+        
+
         conn = sqlite3.connect('./static/data/messages.db')
         curs = conn.cursor()
-        curs.execute("INSERT INTO messages(message) VALUES(?)",(user))
+        curs.execute("INSERT INTO messages (message) VALUES(?)",(message,))
         conn.commit()
-        conn.close
-        return redirect(url_for('success', name = user))
+        conn.close()
+
+       
+        return redirect(url_for('success', message = message))
     else:
-        user = request.args.get('nm')
-        return render_template('login.html', name = user)
+        message = request.args.get('nm')
+        return render_template('login.html', message = message)
+
+@app.route('/all')
+def all():
+    #connect to DB
+    conn = sqlite3.connect('./static/data/messages.db')
+    curs = conn.cursor()
+    messages = []
+    rows = curs.execute("SELECT * from messages")
+    for row in rows:
+        message = {'message':row[0]}
+        messages.append(message)
+    conn.close()
+    return render_template('all.html', messages = messages)
+
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
