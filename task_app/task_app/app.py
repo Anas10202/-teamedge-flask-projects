5697ebbc57c7bc30ad34915b1613a081536974da
Anas10202 <77549785+Anas10202@users.noreply.github.com>
2021-05-14 18:05:26 -0400

    Add files via upload

---
+++ task_app/task_app/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template , redirect, request,url_for,json ,jsonify, current_app as app
+from datetime import date
+from sense_hat import SenseHat
+import requests
+import os
+import sqlite3
+from flask_apscheduler import APScheduler
+
+app = Flask(__name__)
+sense = SenseHat()
+
+@app.route('/',methods=['POST','GET'])
+def remind():
+    if request.method == 'POST':
+        message= request.form['nm']
+        time=request.form['remindertime']
+        sense.show_message(message)
+
+
+        conn = sqlite3.connect('./static/data/reminder.db')
+        curs = conn.cursor()
+        curs.execute("INSERT INTO messages (message,time) VALUES((?),(?))",(message,time))
+        conn.commit()
+        conn.close()
+        
+
+        return render_template('reminder.html', message = message, time=time)
+    else:
+        message = request.args.get('nm')
+        time=request.args.get('remindertime')
+    
+        return render_template('reminder.html', message = message, time=time)
+@app.route('/all')
+def all():
+    #connect to DB
+    conn = sqlite3.connect('./static/data/reminder.db')
+    curs = conn.cursor()
+    messages = []
+    rows = curs.execute("SELECT * from messages")
+    for row in rows:
+        message = {'message':row[0],'time':row[1]}
+        messages.append(message)
+    conn.close()
+    return render_template('all.html', messages = messages)
+
+
+@app.route('/button/delete/<btn>')
+def delete():
+    #connect to DB
+    conn = sqlite3.connect('./static/da ta/reminder.db')
+    curs = conn.cursor()
+    curs.execute("DELETE from messages WHERE (rowid)=VALUES(?)")
+    conn.commit()
+    conn.close()
+    return render_template('all.html')
+
+    
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True, host='0.0.0.0')
